cbf5212b77fba3ad227fa0aeb63e9ee0df2362b7
Tad Fisher <tadfisher@gmail.com>
2019-05-29 15:50:30 -0700

    Fix travis

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,6 @@
 
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
-
-./update.sh
-
 git add --all
 git commit --message "Repo update [$(date --utc --iso-8601)]"
 git remote add upstream "https://${GH_TOKEN}@github.com/tadfisher/android-nixpkgs.git" > /dev/null 2>&1
