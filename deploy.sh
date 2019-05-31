2a59cab7feed2e669aed5a7e5d8dacf1f5877ee0
Tad Fisher <tadfisher@gmail.com>
2019-05-31 14:00:43 -0700

    Re-enable PR/branch tests

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
 
+set -e
+
 git checkout master
+
 ./update.sh
+
+./test.sh
+
 git add *
 tag="$(date --utc --iso-8601)"
 git commit -m "Repo update: $tag"
 git tag --annotate --message="Version $tag" "$tag"
 
-if [ $? -eq 0 ]; then
-  echo "Deploying $tag to master"
-  if [ -n "$TRAVIS" ]; then
+echo "Deploying $tag to master"
+if [ -n "$TRAVIS" ]; then
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis CI"
     git remote rm origin
-    git remote add origin https://tadfisher:${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
-  fi
-  git push --follow-tags origin master
-else
-  echo "Update failed, or nothing to push."
+    git remote add origin "https://tadfisher:${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
 fi
+
+git push --follow-tags origin master
