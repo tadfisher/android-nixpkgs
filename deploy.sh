a0916e41439fe65dac6c48db77d934daae732305
Tad Fisher <tadfisher@gmail.com>
2019-07-09 16:56:37 -0700

    cache nix store; deploy nix channel

--- deploy.sh
+++ deploy.sh
@@ -23,4 +23,10 @@ fi
 
 git push --follow-tags origin master
 
-./cachix.sh
+mkdir -p build
+touch build/index.html
+echo 'https://android.cachix.org' > build/binary-cache-url
+git show-ref refs/heads/master --hash > build/git-revision
+tar -cJf build/nixexprs.tar.xz default.nix channels lib pkgs repo \
+    --transform "s,^,${PWD##*/}/," \
+    --owner=0 --group=0 --mtime="1970-01-01 00:00:00 UTC"
