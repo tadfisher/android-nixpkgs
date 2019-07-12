48a8be58b8665a0b171755d1762a1b0c34ed8812
Tad Fisher <tadfisher@gmail.com>
2019-07-12 13:01:20 -0700

    cachix: don't fill up disk

--- scripts/cachix.sh
+++ scripts/cachix.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i bash -p cachix
+#!nix-shell -i bash -p cachix jq
 
 set -e -x -o pipefail
 
 channel=${1?Usage: $0 channel}
 
-nix-build -Q -I nixpkgs="channel:$channel" | cachix push android
+paths=$(nix-env -qaP -f default.nix '.*' | cut -d ' ' -f1)
+
+for path in $paths; do
+    nix-build -I nixpkgs="channel:$channel" -A $path -Q --no-out-link 2>/dev/null | cachix push android
+    nix-store --gc 2>/dev/null
+done
