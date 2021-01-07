45b5e74cd94fbdfd6028895a759d3f1e78eef2bc
Tad Fisher <tadfisher@gmail.com>
2021-01-07 12:10:29 -0800

    Change default branch to 'main'

--- scripts/channel.sh
+++ scripts/channel.sh
@@ -3,7 +3,7 @@
 mkdir -p build
 touch build/index.html
 echo 'https://android.cachix.org' > build/binary-cache-url
-git show-ref refs/heads/master --hash > build/git-revision
+git show-ref refs/heads/main --hash > build/git-revision
 tar -cJf build/nixexprs.tar.xz default.nix channels pkgs \
     --transform "s,^,${PWD##*/}/," \
     --owner=0 --group=0 --mtime="1970-01-01 00:00:00 UTC"
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,4 @@ git add channels/\*\*
 tag="$(date --utc --iso-8601)"
 git commit -m "Repo update: $tag" || exit 0
 git tag --annotate --message="Version $tag" "$tag"
-git push --follow-tags origin master
+git push --follow-tags origin main
