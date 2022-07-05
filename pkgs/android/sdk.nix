029ef9a7f9af79dd4c63c0ea278776d6abb7a903
Tad Fisher <tadfisher@gmail.com>
2022-07-05 14:20:08 -0700

    sdk: Add installed packages to passthru

--- pkgs/android/sdk.nix
+++ pkgs/android/sdk.nix
@@ -53,6 +53,7 @@ let
         # Android Studio uses this even though it is deprecated.
         export ANDROID_HOME="$ANDROID_SDK_ROOT"
       '';
+      passthru.packages = pkgs;
     } ''
     export ANDROID_SDK_ROOT=$out/share/android-sdk
     mkdir -p "$ANDROID_SDK_ROOT"
