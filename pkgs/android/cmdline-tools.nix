6a7ff3d986e30f62c7c4c2d989e71a62d0ec6700
Sergii Maksymov <ua.maximoff@gmail.com>
2023-09-17 01:57:57 +0200

    fix: use wrapProgram for compatibility with flutter

--- pkgs/android/cmdline-tools.nix
+++ pkgs/android/cmdline-tools.nix
@@ -5,13 +5,16 @@ mkGeneric
   pname = "cmdline-tools";
 
   passthru.installSdk = ''
+    chmod +w $pkgBase/bin
     for script in $pkgBase/bin/*; do
-      makeWrapper $script $out/bin/$(basename $script) \
+      wrapProgram $script \
         --set-default JAVA_HOME "${jdk.home}" \
         --set-default ANDROID_SDK_ROOT $ANDROID_SDK_ROOT \
         --prefix JAVA_OPTS ' ' "-Dcom.android.sdklib.toolsdir=$pkgBase" \
         --prefix JAVA_OPTS ' ' "-Dcom.android.sdkmanager.toolsdir=$pkgBase" \
         --prefix JAVA_OPTS ' ' "-Dcom.android.tools.lint.bindir=$pkgBase"
-      done
+      ln -rvs $script $out/bin/$(basename $script)
+    done
+    chmod -w $pkgBase/bin
   '';
 }
