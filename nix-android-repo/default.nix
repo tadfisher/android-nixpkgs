8715f2d4a7491da46024c04074a3cfed7a04c5f0
Tad Fisher <tadfisher@gmail.com>
2023-12-21 11:43:59 -0800

    Update repo indexer

--- nix-android-repo/default.nix
+++ nix-android-repo/default.nix
@@ -1,10 +1,10 @@
 { lib
-, final
-, prev ? final
+, devshellPkgs
+, pkgs
 }:
 
 let
-  inherit (final) callPackage runCommand;
+  inherit (pkgs) callPackage jdk runCommand;
 
   repos = [
     "https://repo1.maven.org/maven2"
@@ -14,17 +14,27 @@ let
 
   buildMavenRepo = callPackage ./maven-repo.nix { };
 
+  gradle = pkgs.gradle.override {
+    java = jdk;
+    javaToolchains = [ jdk ];
+  };
+
 in
 rec {
+  devshell = callPackage ./devshell.nix {
+    inherit devshellPkgs gradle gradle-properties jdk update-locks;
+  };
+
   gradle-properties = runCommand "gradle.properties"
     {
       mavenRepo = "file://${maven-repo}";
+      jdk = lib.versions.major (lib.getVersion jdk);
     } ''
     substituteAll ${./gradle.properties.in} $out
   '';
 
   nix-android-repo = callPackage ./nix-android-repo.nix {
-    inherit maven-repo;
+    inherit gradle jdk maven-repo;
   };
 
   maven-repo = buildMavenRepo {
@@ -34,8 +44,8 @@ rec {
   };
 
   update-locks = callPackage ./update-locks.nix {
-    inherit (final.haskellPackages) xml-to-json-fast;
-    inherit repos;
+    inherit (pkgs.haskellPackages) xml-to-json-fast;
+    inherit gradle repos;
     fetchSources = true;
   };
 }
