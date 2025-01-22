2120d93d4158c8194403d9e8778e3c2254ce034a
Tad Fisher <tadfisher@gmail.com>
2025-01-22 14:58:15 -0800

    Bump dependencies

--- nix-android-repo/default.nix
+++ nix-android-repo/default.nix
@@ -1,51 +1,24 @@
-{ lib
-, devshellPkgs
+{ devshellPkgs
+, gradle2nixBuilders
+, gradle2nixPkgs
 , pkgs
 }:
 
 let
-  inherit (pkgs) callPackage jdk runCommand;
-
-  repos = [
-    "https://repo1.maven.org/maven2"
-    "https://dl.google.com/dl/android/maven2"
-    "https://plugins.gradle.org/m2"
-  ];
-
-  buildMavenRepo = callPackage ./maven-repo.nix { };
-
-  gradle = pkgs.gradle.override {
-    java = jdk;
-    javaToolchains = [ jdk ];
-  };
-
+  inherit (gradle2nixBuilders) buildGradlePackage;
+  inherit (gradle2nixPkgs) gradle2nix;
+  inherit (pkgs) callPackage jdk;
 in
 rec {
   devshell = callPackage ./devshell.nix {
-    inherit devshellPkgs gradle gradle-properties jdk update-locks;
+    inherit devshellPkgs gradle2nix jdk update-locks;
   };
-
-  gradle-properties = runCommand "gradle.properties"
-    {
-      mavenRepo = "file://${maven-repo}";
-      jdk = lib.versions.major (lib.getVersion jdk);
-    } ''
-    substituteAll ${./gradle.properties.in} $out
-  '';
 
   nix-android-repo = callPackage ./nix-android-repo.nix {
-    inherit gradle jdk maven-repo;
-  };
-
-  maven-repo = buildMavenRepo {
-    inherit repos;
-    deps = builtins.fromJSON (builtins.readFile ./deps.json);
-    fetchSources = true;
+    inherit buildGradlePackage jdk;
   };
 
   update-locks = callPackage ./update-locks.nix {
-    inherit (pkgs.haskellPackages) xml-to-json-fast;
-    inherit gradle repos;
-    fetchSources = true;
+    inherit gradle2nix;
   };
 }
